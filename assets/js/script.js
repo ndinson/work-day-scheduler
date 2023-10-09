6a2bb7ea9892ed27248daea780924a4c82ad9e53
Nainoa Dinson <nd808@msn.com>
2023-10-08 15:43:09 -1000

    added time-blocks for the remaining hours in the 9-5 work day

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,15 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+  dayjs().day()
+
+
+  var saveBtnEl = $(".saveBtn")
+
+
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
